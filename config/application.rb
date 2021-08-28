f85f68a00389821c2907a431b17d0f53ceb5f1e4
Urchmaney <kingsobino@gmail.com>
2021-08-28 09:36:05 +0000

    feat: Fix all rubocop lint errors

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ require 'rails/test_unit/railtie'
 Bundler.require(*Rails.groups)
 
 module Mindfullness
+  # Rail Application
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
