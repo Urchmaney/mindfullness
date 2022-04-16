fc88cb2894d193f82a024630b3841012d1a20c6d
kingsley-people-box <kingsley@peoplebox.ai>
2022-04-16 09:13:53 +0100

    fix: migration issues

--- db/migrate/20220415040205_create_session.rb
+++ db/migrate/20220415040205_create_session.rb
@@ -1,4 +1,4 @@
-class Session < ActiveRecord::Migration[6.1]
+class CreateSession < ActiveRecord::Migration[6.1]
   def change
     create_table :sessions do |t|
       t.string :title
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_28_114959) do
+ActiveRecord::Schema.define(version: 2022_04_15_042541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.string "statement"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "session_helpers", force: :cascade do |t|
+    t.string "full_name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "session_questions", force: :cascade do |t|
+    t.bigint "session_id"
+    t.bigint "question_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_session_questions_on_question_id"
+    t.index ["session_id"], name: "index_session_questions_on_session_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "full_name", null: false
@@ -22,15 +52,7 @@ ActiveRecord::Schema.define(version: 2021_08_28_114959) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "worries", force: :cascade do |t|
-    t.text "description"
-    t.string "recording_url"
-    t.string "title", null: false
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_worries_on_user_id"
-  end
-
-  add_foreign_key "worries", "users"
+  add_foreign_key "session_questions", "questions"
+  add_foreign_key "session_questions", "sessions"
+  add_foreign_key "sessions", "users"
 end
